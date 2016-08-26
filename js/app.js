27105a31254f05ddb9b00e82b37d286e52303c80
margotsavin <margotsavin@gmail.com>
2016-08-26 14:57:41 +0200

    zapocet portfolio

--- js/app.js
+++ js/app.js
@@ -15,24 +15,67 @@ var app = angular.module('site', []);
 	});
 
 	app.controller('PortfolioController', function() {
-		this.portfolio= projekti;
+		this.projekti= projekti;
 	});
 
 
 	var projekti = [
 	{
 		nazivProjekta:'Proba',
+		opisProjekta:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. N ',
 		slike: [
 			{
 				naziv:'medved',
-				jpg:'../img/portfolio/1.jpg'
+				jpg:'./img/portfolio/1.jpg'
 			},
 			{
 				naziv:'vuk',
-				jpg:'../img/portfolio/2.jpg'
+				jpg:'./img/portfolio/2.jpg'
 			}
 		]
-	}
+	},
+	{
+		nazivProjekta:'Proba2',
+		opisProjekta:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id purus justo. ',
+		slike: [
+			{
+				naziv:'medved',
+				jpg:'./img/portfolio/2.jpg'
+			},
+			{
+				naziv:'vuk',
+				jpg:'./img/portfolio/1.jpg'
+			}
+		]
+	},
+	{
+		nazivProjekta:'Proba3',
+		opisProjekta:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id purus justo.  ',
+		slike: [
+			{
+				naziv:'medved',
+				jpg:'./img/portfolio/3.jpg'
+			},
+			{
+				naziv:'vuk',
+				jpg:'./img/portfolio/2.jpg'
+			}
+		]
+	},
+	{
+		nazivProjekta:'Proba4',
+		opisProjekta:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pretium enim at purus. ',
+		slike: [
+			{
+				naziv:'medved',
+				jpg:'./img/portfolio/1.jpg'
+			},
+			{
+				naziv:'vuk',
+				jpg:'./img/portfolio/2.jpg'
+			}
+		]
+	},
 
 	];
 
